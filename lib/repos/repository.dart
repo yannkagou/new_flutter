ba6518ebe9025a1255968dd1aff604e5806bb36e
unknown <yannickkagou@gmail.com>
2024-02-22 07:12:59 +0100

    Add a tabBar in a dialog after the pull

--- lib/repos/repository.dart
+++ lib/repos/repository.dart
@@ -1,15 +1,15 @@
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:http/http.dart';
+// import 'package:http/http.dart';
 
-class UserRepository {
-  String endpoint = 'https://reqres.in/api/users?page=2';
-  getUsers() async {
-    Response response = await get(Uri.parse(endpoint));
-    if (response.statusCode == 200){
-      final List result = jsonDecode(response.body)['data'];
-    } else {
-      throw Exception(response.reasonPhrase);
-    }
-  }
-}
+// class UserRepository {
+//   String endpoint = 'https://reqres.in/api/users?page=2';
+//   getUsers() async {
+//     Response response = await get(Uri.parse(endpoint));
+//     if (response.statusCode == 200){
+//       final List result = jsonDecode(response.body)['data'];
+//     } else {
+//       throw Exception(response.reasonPhrase);
+//     }
+//   }
+// }
